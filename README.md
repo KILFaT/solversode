b27c694403ded322acf3cd63e03cad82d48b7f5c
Ilfat Kinyaev <kinyaevilfat@gmail.com>
2018-02-16 14:27:31 +0300

    Add test basic usage of mpmath lib

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # solversode
 
 ## Save dependencies
-python pip freeze > requirements.txt
+python -m pip freeze > requirements.txt
 
 ## Download dependencies to local environment
 python -v venv ./
 ./Scripts/activate
-python pip install -r requirements.txt
+python -m pip install -r requirements.txt
--- solversode.py
+++ solversode.py
@@ -4,15 +4,23 @@ from src import module
 from src.function_timer import fn_timer
 import timeit
 import random
-import mpmath
+from mpmath import mp
 
-print("Started")
 
 @fn_timer
 def random_sort(n):
     return sorted([random.random() for i in range(n)])
- 
- 
+
+
+@fn_timer
+def get_sqrt(n):
+    mp.dps = 1000
+    x = mp.sqrt(n)
+    print(x)
+    return
+
+
 if __name__ == "__main__":
- random_sort(20000)
- random_sort(123341)
+    print("Started")
+    get_sqrt(2)
+    print("Finished")
